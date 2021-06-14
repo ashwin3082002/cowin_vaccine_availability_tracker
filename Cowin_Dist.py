ad2f101daa7ddb0db20c2204ec4abb69945cf389
R Ashwin <73417773+ashwin3082002@users.noreply.github.com>
2021-06-14 22:10:54 +0530

    Add files via upload

---
+++ Cowin_Dist.py
@@ -0,0 +1,37 @@
+from tkinter.constants import E
+import requests
+import time
+from playsound import playsound
+from tkinter import messagebox
+dist = 571
+date = '14-06-2021'
+URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(
+    dist, date)
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+
+def findAvailability():
+    counter = 0
+    result = requests.get(URL, headers=header)
+    response_json = result.json()
+    data = response_json["sessions"]
+    for each in data:
+        if((each["available_capacity_dose2"] > 0) & (each["min_age_limit"] == 18) and (each["vaccine"] == 'COVAXIN') and (each["name"]!='Rainbow Childrens Medicare' )):
+            counter += 1
+            print(each["name"])
+            est=each["name"]
+            print(each["pincode"])
+            print(each["vaccine"])
+            print(each["available_capacity_dose2"])
+            messagebox.showinfo("INFO", "Vaccine Found at:  "+est)
+        
+    if(counter == 0):
+        print("No Available Slots")
+        return False
+
+
+while(findAvailability() != True):
+    time.sleep(5)
+    findAvailability()
